8d349df3443d45d84225d98d3e77a0a2a316dac3
Martin Morales <you@example.com>
2018-11-13 00:40:42 -0700

    initial commit

---
+++ src/main/java/com/states/StatesServiceApplication.java
@@ -0,0 +1,12 @@
+package com.states;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatesServiceApplication.class, args);
+	}
+}
